6a1fe01457d497fc4e0b6764f0b7307dd7cb21e1
rmoore <rem516@gmail.com>
2026-09-07 22:38:22 -0500

    chore: remove no longer needed command line runner

--- backend/src/main/java/xyz/raymoore/App.java
+++ backend/src/main/java/xyz/raymoore/App.java
@@ -1,24 +1,12 @@
 package xyz.raymoore;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import xyz.raymoore.madisonsc.repository.ContestantRepository;
 
 @SpringBootApplication
 public class App {
 
     static void main(String[] args) {
         SpringApplication.run(App.class, args);
-    }
-
-    @Bean
-    CommandLineRunner listContestants(ContestantRepository repository) {
-        return args -> {
-            var contestants = repository.findAllAlphabetically();
-            System.out.println("Found " + contestants.size() + " contestant(s).");
-            contestants.forEach(System.out::println);
-        };
     }
 }
